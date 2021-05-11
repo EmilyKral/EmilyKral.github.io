965146b5de902d66903f0566f2439733e51da31d
Emily Kral <78175461+EmilyKral@users.noreply.github.com>
2021-05-11 11:58:13 +0100

    commenting js script

--- Resources/JS/script.js
+++ Resources/JS/script.js
@@ -1,3 +1,4 @@
+// variables for the photos that will appear and the clickable words for each
 let isleOfWight = document.getElementById("isleofwight");
 let isleOfWightLink = document.getElementById("isleofwight-link");
 let graduate = document.getElementById("graduate");
@@ -12,22 +13,24 @@ let swimming = document.getElementById("swimming");
 let swimmingLink = document.getElementById("swimming-link");
 let darkLayer = document.getElementById("dark-layer");
 
+// array of all photos
 let photos = [isleOfWight, graduate, spain, cakes, beach, swimming];
 
 function makeVisible(target) {
-	target.style.display = "flex";
-	darkLayer.style.display = "inline";
+	target.style.display = "flex"; // makes targeted photo appear
+	darkLayer.style.display = "inline"; // makes background dark behind photo
 }
 
 function makeInvisible() {
-	darkLayer.style.display = "none";
+	darkLayer.style.display = "none"; // removes dark background
 	for (let i = 0; i < photos.length; i++) {
 		if (photos[i].style.display == "flex") {
-			photos[i].style.display = "none";
+			photos[i].style.display = "none"; // checks through all photos and removes photo if it is visible
 		}
 	}
 }
 
+// making all the links separately clickable and to cause the right photo to appear
 isleOfWightLink.onclick = function () {
 	makeVisible(isleOfWight);
 };
@@ -52,6 +55,7 @@ swimmingLink.onclick = function () {
 	makeVisible(swimming);
 };
 
+// when the dark background (and so also a photo) is displayed, clicking anywhere will make the photo disappear
 darkLayer.onclick = function () {
 	makeInvisible();
 };
